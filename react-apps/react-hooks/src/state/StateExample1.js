22b97dc8ccdc2ef6d0e9e4748654e271ef1364d2
rkpatra201 <rabindrapatra825@gmail.com>
2023-07-02 07:28:06 +0530

    updated state

--- react-apps/react-hooks/src/state/StateExample1.js
+++ react-apps/react-hooks/src/state/StateExample1.js
@@ -1,15 +1,15 @@
 
 
 export const StateExample1 = ()=>{
-
+  // not using state. 
+  // so even though count increase with each click, no re-rendering will happen.
     let count = 0;
     const countHandler = ()=>{
         count = count +1;
-        console.log(count);
+        console.log(count); // increment of count wont be visible.
     }
     return <>
       <h1>State Example 1</h1>
-      
       <button onClick={countHandler}>Increase Count</button>{count}
       <hr />
     </>
--- react-apps/react-hooks/src/state/StateExample2.js
+++ react-apps/react-hooks/src/state/StateExample2.js
@@ -2,17 +2,16 @@ import { useState } from "react";
 
 
 export const StateExample2 = ()=>{
+    // array destructuring
+    let [count,setCount] = useState(10); // initialize count with 10
 
-    let [count,setCount] = useState(10);
-    
     const countHandler = ()=>{
-       setCount((prevState)=>{
+       setCount((prevState)=>{ // setCount as a higher order function
         return prevState + 1;
        });
     }
     return <>
       <h1>State Example 2</h1>
-      
       <button onClick={countHandler}>Increase Count</button>{count}
       <hr />
     </>
